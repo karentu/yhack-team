b5b961eabfc987f40ea7a87b9d116c0448f30b72
Karen <karen_tu@brown.edu>
2017-12-03 02:22:23 -0500

    post request enabled

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+$(function() {
+    $('button').click(function(e) {
+        var user = $('#txtUsername').val();
+        var pass = $('#txtPassword').val();
+        $.ajax({
+            url: '/signUpUser',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
+
+$.ajax({
+	type: "Post",
+	url: "~/find_news.py",
+
+})
 getData(x, y) {
 	....
 
